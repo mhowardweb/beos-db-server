db795f17501395b2c78a203df186dbda0af367a5
Matthew <mhoward181@outlook.com>
2019-06-19 13:28:08 +0100

    Add port

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ fastify.register(gql, {
 });
 
 // Run the server!
+// You need to set process.env.PORT for heroku to set the port dynamically
 const start = async () => {
   try {
-    await fastify.listen(3000, "0.0.0.0");
+    await fastify.listen({ port: process.env.PORT || 4000 });
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
